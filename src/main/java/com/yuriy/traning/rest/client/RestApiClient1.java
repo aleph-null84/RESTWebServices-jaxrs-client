d2f4eab155a35f59f5b29000737c638c314457f1
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-07 20:08:25 +0300

    lesson #16

--- src/main/java/com/yuriy/traning/rest/client/RestApiClient1.java
+++ src/main/java/com/yuriy/traning/rest/client/RestApiClient1.java
@@ -2,6 +2,7 @@ package com.yuriy.traning.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.yuriy.traning.messenger.model.Message;
@@ -11,6 +12,10 @@ public class RestApiClient1 {
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
+		
+		WebTarget baseTarget = client.target("http://localhost:8080/jaxrs-client/webapi/");
+		WebTarget messagesTarget = baseTarget.path("messages"); 
+		WebTarget singleMessageTarget = messagesTarget.path("{messageId}"); 
 		
 		Response response = client.target("http://localhost:8080/jaxrs-client/webapi/messages/1")
 				                  .request()
