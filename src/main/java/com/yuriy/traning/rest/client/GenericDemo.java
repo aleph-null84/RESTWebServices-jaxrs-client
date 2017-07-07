37e55b26933fbd1d9b62fdf4655cefc5f2c06d19
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-07 20:54:00 +0300

    lesson #19

--- src/main/java/com/yuriy/traning/rest/client/GenericDemo.java
+++ src/main/java/com/yuriy/traning/rest/client/GenericDemo.java
@@ -6,7 +6,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.yuriy.traning.messenger.model.Message;
 
@@ -19,8 +18,7 @@ public class GenericDemo {
 				                       .path("messages")
 				                       .queryParam("year", 2015)
 				                       .request(MediaType.APPLICATION_JSON)
-				                       .get(new GenericType<List<Message>>() {
-				});
+				                       .get(new GenericType<List<Message>>() {});
 		System.out.println(messages);
 
 	}
